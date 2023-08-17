1d49b22148e3965386afb830c9ce08bb610ac43a
Lucas Garcia <lucas.garcia@voidsoftware.com>
2023-08-17 23:25:18 +0100

    Added favico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Talentier',
+    title: 'Talentier - Finding jobs and talents has never been easier.',
     description: ' A platform built for a new way of finding jobs.',
     viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
     keywords: 'Jobs, Find Job, Talent, Organization, Job seeker, Recruiter',
